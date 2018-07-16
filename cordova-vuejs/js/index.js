a56b2c616130dc19de0d5b0f0d00d5c5434c4875
Vinicius Rufine <vsrufine@gmail.com>
2018-07-16 19:55:58 -0300

    basic routing

--- cordova-vuejs/js/index.js
+++ cordova-vuejs/js/index.js
@@ -1,12 +1,45 @@
-var vueApp = new Vue({
-  el: '#app',
-  data: {
-    message: 'MobileUI + VueJS = ',
-    customStyle: 'text-align: center; font-family: sans-serif'
+var HomeComponent = Vue.component('home', {
+  name: 'Home',
+  data: function () {
+    return {
+      message: 'MobileUI + VueJS = '
+    }
   },
   template: `
-  <div :style="customStyle">
-    <h1>{{ message }} <span style="color: red">❤️</span></h1>
-  </div>
+    <div>
+      <h1>{{ message }} <span style="color: red">❤️</span></h1>
+    </div>
   `
+})
+
+var DemoRouterComponent = Vue.component('basic', {
+  data: function () {
+    return {
+      title: 'You can use VueRouter!'
+    }
+  },
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt voluptatum numquam, molestias illo ut aliquid dolore? Ut sed commodi labore ullam molestias quae quam eum aliquam. Adipisci nisi fuga deleniti.</p>
+    </div>
+  `
+})
+
+var router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: HomeComponent
+    },
+    {
+      path: '/demo',
+      component: DemoRouterComponent
+    }
+  ]
+})
+
+var vueApp = new Vue({
+  el: '#app',
+  router
 })
